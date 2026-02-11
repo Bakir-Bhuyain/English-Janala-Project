b59ab4773732e53f7400b2bacfeaa3fe6907b2e6
Bakir-Bhuyain <hemalbakir@gmail.com>
2026-02-11 20:28:01 +0600

    added an demo modal

--- script/script.js
+++ script/script.js
@@ -19,28 +19,40 @@ const loadLevelWord = async (id) => {
   // console.log(url);
   const loadWord = await url.json();
   // console.log(loadWord.data);
+//   const clickBtn = document.getElementById(`lesson-btn-${id}`);
+//   clickBtn.classList.add("active");
   displayLevelWord(loadWord.data);
-  // console.log(data);
+  // console.log(data); 
 };
 
 const displayLevelWord = (words) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
   // console.log(levelContainer);
+  if (words.length === 0) {
+    wordContainer.innerHTML = `
+      <div class="text-center col-span-full rounded py-10 bangla-font">
+      <img class="mx-auto" src ="./assets/alert-error.png">
+        <p class=" font-poppins text-gray-400 ">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।,</p>
+        <h2 class="text-2xl bangla-font font-semibold">নেক্সট Lesson এ যান।</h2>
+      </div>
+`;
+    return;
+  }
   words.forEach((word) => {
-      console.log(word)
-      const card = document.createElement('div');
-      card.innerHTML = `
+    console.log(word);
+    const card = document.createElement("div");
+    card.innerHTML = `
       <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-        <h2 class="text-2xl font-bold">${word.word}</h2>
+        <h2 class="text-2xl font-bold">${word.word ? word.word : "শব্দ পাওয়া যায় নি!!"}</h2>
         <p class="font-poppins text-2xl">Meaning/ Pronunciation</p>
-        <div class="font-bangla text-2xl">"${word.meaning} / ${word.pronunciation}"</div>
-        <div class="flex justify-between items-center">
+        <div class="font-bangla text-2xl">"${word.meaning ? word.meaning : "অর্থপাওয়া যায় নি!!"} / ${word.pronunciation ? word.pronunciation : "pronunciation পাওয়া যায় নি!!"}"</div>
+        <div onclick="my_modal_2.showModal()" class="flex justify-between items-center">
         <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF50]"><i class="fa-solid fa-circle-info"></i></button>
         <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF50]"><i class="fa-solid fa-volume-high"></i></button>
         </div>
       </div>`;
-      wordContainer.appendChild(card)
+    wordContainer.appendChild(card);
   });
 };
 
@@ -54,7 +66,7 @@ const displayLesson = (lessons) => {
     // 3. create element
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
-    <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
+    <button id="lesson-btn-${lesson.level_no}" onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
     <i class="fa-solid fa-circle-question"></i>LESSONS-${lesson.level_no}
     </button>
     `;
